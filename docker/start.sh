357ca0c2067ec6c15c57d0ee1fab01975d055d55
paul.j.alkemade@gmail.com <fetchbot@h9.vx.rmit.edu.au>
2019-11-10 20:36:20 +1100

    run processes in parallel

--- docker/start.sh
+++ docker/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-roslaunch tracking single_camera_tracking_node.launch enable_pose:=true enable_people_tracking:=true enable_object:=false
-python /HAE/fetchbot/openptrack_impl.py
+roslaunch tracking single_camera_tracking_node.launch enable_pose:=true enable_people_tracking:=true enable_object:=false \
+&& python /HAE/fetchbot/openptrack_impl.py
