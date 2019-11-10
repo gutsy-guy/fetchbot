baca1d5e6f7f9c3ee910cf85ee5552c46242d2dc
paul.j.alkemade@gmail.com <fetchbot@h9.vx.rmit.edu.au>
2019-11-10 21:33:54 +1100

    updated start script

--- docker/start.sh
+++ docker/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-roslaunch tracking single_camera_tracking_node.launch enable_pose:=true enable_people_tracking:=true enable_object:=false & python /root/HAE/docker/openptrack_impl.py
+roslaunch tracking single_camera_tracking_node.launch enable_pose:=true enable_people_tracking:=true enable_object:=false & python /root/HAE/openptrack_impl.py
